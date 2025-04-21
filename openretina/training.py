93a9fb42af2795b48c7a241d342ab4fc0b69726e
Polina Turishcheva <pturishcheva@gmail.com>
2025-04-21 16:36:32 +0200

    add dec to the train loop

--- openretina/training.py
+++ openretina/training.py
@@ -16,6 +16,8 @@ from .cyclers import LongCycler
 from .early_stopping import early_stopping
 from .tracking import MultipleObjectiveTracker
 from .utils.misc import set_seed
+from sklearn.cluster import KMeans
+from torch.nn import KLDivLoss
 
 
 def standard_early_stop_trainer(
@@ -42,8 +44,133 @@ def standard_early_stop_trainer(
     detach_core: bool = False,
     wandb_logger=None,
     cb=None,
+    include_kldivergence=True,
+    cluster_number=10,
+    alpha=1.0,
+    dec_starting_epoch=5,
+    kmeans_init=20,
+    base_multiplier=4e3,
+    subsamples=2000,
+    use_diag_cov=True,
+    # learn_alpha=False,
+    exponent=2,
+    # load_pretrain=False,
+    # load_adlognorm=True,
+    # pretrained_epoch=30,
     **kwargs,
 ):
+    
+    def get_multiplier(epoch, base_multiplier=4e3):
+        """Multiplier to scale KL loss in same order of magnitude as main loss
+        To avoid hard peek aat starting epoch we include a warm-up phase s.t. the loss can increase slower
+        """
+        if epoch < dec_starting_epoch:
+            return 0
+        else:
+            return base_multiplier
+        """elif dec_warumup_epoch == dec_starting_epoch:
+            return base_multiplier
+            elif dec_warumup_epoch >= epoch >= dec_starting_epoch:
+            return (
+                base_multiplier
+                * (epoch - dec_starting_epoch)
+                / (dec_warumup_epoch - dec_starting_epoch)
+            )"""
+
+    def soft_assignments(encoded_features, cluster_centers, alpha=alpha):
+        """
+        Compute soft assingments q_ij as described in DEC paper (1)
+        q_ij = (1+ ||z_i - \mu_j||^2/a)^(-(a+1)/2) / (sum_j'((1+ ||z_i - \mu_j'||^2/a)^(-(a+1)/2)))
+        """
+        norm_squared = torch.sum(
+            (encoded_features.T.unsqueeze(1) - cluster_centers.unsqueeze(0)) ** 2, 2
+        )
+        assignments = 1.0 / (1.0 + (norm_squared / alpha))
+        assignments = assignments ** ((alpha + 1) / 2)
+        return assignments / torch.sum(assignments, dim=1, keepdim=True)
+
+    def target_distribution(batch: torch.Tensor, exponent=exponent) -> torch.Tensor:
+        """
+        Compute the target distribution p_ij, given the batch (q_ij), as in 3.1.3 Equation 3 of
+        Xie/Girshick/Farhadi; this is used the KL-divergence loss function.
+        p_ij = (q_ij^2/f_j) / sum_j'(q_ij'^2/f_j')  f_j =sum_i(q_ij)
+
+        :param batch: [batch size, number of clusters] Tensor of dtype float
+        :return: [batch size, number of clusters] Tensor of dtype float
+        """
+        weight = (batch**exponent) / torch.sum(batch, 0)
+        return (weight.t() / torch.sum(weight, 1)).t()
+
+
+    def soft_assignments_mult(encoded_features, cluster_centers, sigma, alpha, p=1):
+        sigma_inv = 1.0 / sigma  # (K, D)
+        diff = encoded_features.T.unsqueeze(1) - cluster_centers.unsqueeze(0)  # (N, K, D)
+        norm_sigma = torch.sum(diff * sigma_inv * diff, dim=2)  # (N, K)
+        det = torch.sum(torch.log(sigma), dim=1)  # log(det) since sigma is diagonal
+        log_gamma_top = torch.lgamma((alpha + p) / 2)
+        log_gamma_bottom = torch.lgamma(alpha / 2)
+        # Log-density formula for multivariate Student-t
+        log_pdf = (
+            log_gamma_top
+            - log_gamma_bottom
+            - 0.5 * det
+            - (p / 2) * torch.log(alpha * torch.pi)
+            - ((alpha + p) / 2) * torch.log(1 + (norm_sigma / alpha))
+        )
+        #log_pdf_max = torch.max(log_pdf, dim=1, keepdim=True)[0]  # Get max per row
+
+        log_assignments = log_pdf - torch.logsumexp(log_pdf, dim=1, keepdim=True)
+        return torch.exp(log_assignments)  # Convert log-assignments to probabilities
+
+    def EM_t_mult(features, resp, cluster_centers, sigma, alpha, d=1):
+        sigma_inv = 1.0 / sigma  # (K,)
+        diff = features.T.unsqueeze(1) - cluster_centers.unsqueeze(0)
+        norm_sigma = torch.sum((diff**2 * sigma_inv), 2)
+        u = ((alpha + d) / (alpha + norm_sigma)).detach()  # ccalculate U shape(N,K)
+        
+        """ M step """
+        numerator = torch.matmul(features, resp * u).T.detach()
+        denominator = torch.sum(resp * u, dim=0, keepdim=True).T.detach()
+        cluster_centers = numerator / denominator
+
+        weighted_sq_diff = resp.unsqueeze(2) * u.unsqueeze(2) * (diff**2)  # (N, K, D)
+        numerator = weighted_sq_diff.sum(dim=0)  # (K,D)
+        denominator = torch.sum(resp, dim=0, keepdim=True)  # (K,)
+        sigma = (numerator / denominator.T).detach()
+
+        sigma = torch.clamp(sigma, min=1e-4, max=1e4)
+
+        return cluster_centers, sigma
+
+    def EM_t_1D(features, resp, cluster_centers, taus, alpha, d=1):
+        norm_squared = torch.sum(
+            (features.T.unsqueeze(1) - cluster_centers.unsqueeze(0)) ** 2, dim=2
+        )
+        u = (alpha + d) / (
+            alpha + norm_squared * (taus ** (-1))
+        )  # ccalculate U shape(N,K)
+        print("u", u)
+
+        """ M step """
+        numerator = torch.matmul(features, resp * u).T.detach()
+        # print(numerator.shape)
+        denominator = torch.sum(resp * u, dim=0, keepdim=True).T.detach()
+        print("denom cc", denominator)
+        cluster_centers = numerator / denominator
+
+        weighted_sums = torch.sum(resp * u * norm_squared, dim=0)
+        taus = (weighted_sums / torch.sum(resp, dim=0, keepdim=True)).detach()
+        print("Tau", taus)
+        return cluster_centers, taus
+
+    def soft_assignments_1D(encoded_features, cluster_centers, tau, alpha=1):
+        norm_squared = torch.sum(
+            (encoded_features.T.unsqueeze(1) - cluster_centers.unsqueeze(0)) ** 2, 2
+        )
+        assignments = 1.0 / (1.0 + (norm_squared / (alpha * tau)))
+        assignments = (assignments ** ((alpha + 1) / 2)) / (tau**1 / 2)
+        return assignments / torch.sum(assignments, dim=1, keepdim=True)
+    
     # Defines objective function; criterion is resolved to the loss_function that is passed as input
     def full_objective(model, data_key, inputs, targets, detach_core):
         regularizers = int(not detach_core) * model.core.regularizer() + model.readout.regularizer(data_key)
@@ -68,11 +195,16 @@ def standard_early_stop_trainer(
     set_seed(seed)
     model.train()
 
+    kldiv_criterion = KLDivLoss(
+        size_average=False
+    )  # losses are summed for each minibatch
+
     criterion = getattr(measures, loss_function)()
     stop_closure = partial(getattr(metrics, stop_function), model, valloaders, device=device)
 
     n_iterations = len(LongCycler(trainloaders))
 
+    alpha = torch.tensor(alpha, device=device, requires_grad=False)
     optimizer = torch.optim.Adam(model.parameters(), lr=lr_init)
     scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
         optimizer,
@@ -101,6 +233,9 @@ def standard_early_stop_trainer(
         tracker_dict.update(model.tracked_values)
 
     tracker = MultipleObjectiveTracker(**tracker_dict)
+    
+    kldiv_list = []
+    print("Alpha: ", alpha)
 
     # train over epochs
     for epoch, val_obj in tqdm(
@@ -123,6 +258,49 @@ def standard_early_stop_trainer(
         position=0,
         leave=True,
     ):
+        if include_kldivergence and epoch == dec_starting_epoch:
+            cluster_centers_list = []
+            kmeans = KMeans(
+                n_clusters=cluster_number, n_init=kmeans_init, random_state=seed
+            )
+            feature_list = []
+            # form initial cluster centres
+            with torch.no_grad():
+                for k, readout in model.readout.items():
+                    features = readout.features.cpu().detach().squeeze().T.numpy()
+                    feature_list.append(np.array(features))
+
+                features = np.vstack(feature_list)
+                predicted = kmeans.fit_predict(features)
+
+            cluster_centers = torch.tensor(
+                kmeans.cluster_centers_, dtype=torch.float, device=device
+            )
+            if use_diag_cov:
+                p = features.shape[1]
+                sigma = torch.zeros((cluster_number, p), device=device)
+                for k in range(cluster_number):
+                    cluster_points = torch.from_numpy(features[predicted == k]).to(
+                        device
+                    )
+                    print(f"Points for cluster {k}: {cluster_points.shape[0]}")
+                    if len(cluster_points) > 0:
+                        sigma[k] = (
+                            torch.var(cluster_points, dim=0, unbiased=True) + 1e-6
+                        )
+
+            else:
+                sigma = torch.zeros(cluster_number, device=device)
+                for k in range(cluster_number):
+                    cluster_points = torch.from_numpy(features[predicted == k]).to(
+                        device
+                    )
+                    if len(cluster_points) > 0:
+                        sigma[k] = torch.mean(
+                            torch.sum((cluster_points - cluster_centers[k]) ** 2, 1)
+                        )
+                sigma = sigma.unsqueeze(0)
+
         # print the quantities from tracker
         if verbose and tracker is not None:
             print("=======================================")
@@ -147,6 +325,51 @@ def standard_early_stop_trainer(
             loss = full_objective(model, clean_data_key, *data, detach_core)  # type: ignore
             loss.backward()
             if (batch_no + 1) % optim_step_count == 0:
+                if include_kldivergence and epoch >= dec_starting_epoch:
+                    kldiv_loss = torch.zeros(1).to(device)
+                    feature_list = []
+                    for k, readout in model.readout.items():
+                        features = readout.features.squeeze()
+                        feature_list.append(features)
+                    feature_list = torch.cat(feature_list, dim=1)
+                    if use_diag_cov:
+                        q = soft_assignments_mult(
+                            feature_list, cluster_centers, sigma, alpha, p
+                        )
+                    else:
+                        q = soft_assignments_1D(
+                            feature_list, cluster_centers, sigma, alpha
+                        )
+                    target = target_distribution(q, exponent)
+                    target = target.clamp(min=1e-10)
+                    q = q.clamp(min=1e-10)
+
+                    kldiv_loss = get_multiplier(epoch, base_multiplier) * (
+                        kldiv_criterion(q.log(), target)
+                    )
+
+                    # To avoid underflow issues when computing this quantity, this loss expects the argument input in the log-space.
+                    # https://pytorch.org/docs/stable/generated/torch.nn.KLDivLoss.html
+                    kldiv_loss.backward()
+                    epoch_loss_kldiv += kldiv_loss.detach()
+                    epoch_loss_kldiv_without_scaling += (
+                        kldiv_loss.detach() / get_multiplier(epoch, base_multiplier)
+                    )
+                    epoch_loss += kldiv_loss.detach()
+                    with torch.no_grad():
+                        cluster_centers_list.append(cluster_centers.cpu().detach())
+                        kldiv_list.append(
+                            kldiv_loss.cpu() / get_multiplier(epoch, base_multiplier)
+                        )
+
+                    if use_diag_cov:
+                        cluster_centers, sigma = EM_t_mult(
+                            feature_list, q, cluster_centers, sigma, alpha, p
+                        )
+                    else:
+                        cluster_centers, sigma = EM_t_1D(
+                            feature_list, q, cluster_centers, sigma, alpha
+                        )
                 optimizer.step()
                 optimizer.zero_grad()
             if np.isnan(loss.item()):
@@ -166,6 +389,20 @@ def standard_early_stop_trainer(
 
     # Model evaluation
     model.eval()
+    if include_kldivergence:
+        soft_assignments_list = []
+        for k, readout in model.readout.items():
+            features = readout.features.detach().squeeze()
+            soft_assignments_list.append(
+                soft_assignments_mult(features, cluster_centers, sigma, alpha, p)
+            )
+        predicted = torch.cat(soft_assignments_list).max(1)[1]
+        # append final cluster_centers
+        cluster_centers_list.append(cluster_centers.cpu().detach().numpy())
+        cluster_centers_np = np.array(cluster_centers_list)
+        kldiv_list_np = np.array(kldiv_list)
+        print("Alpha: ", alpha)
+
     tracker.finalize()
 
     # Compute avg validation and test correlation
@@ -174,6 +411,9 @@ def standard_early_stop_trainer(
 
     # return the whole tracker output as a dict
     output = tracker.asdict()
+
+    if include_kldivergence:
+        return avg_test_corr, avg_val_corr, output, cluster_centers_np, predicted, model.state_dict()
 
     return avg_test_corr, avg_val_corr, output, model.state_dict()
 
